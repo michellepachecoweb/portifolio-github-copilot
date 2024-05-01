450b9a536167ae14965ddaf1067f0d0bb5a307f5
michellepachecoweb <michellepachecoweb@gmail.com>
2024-05-01 20:51:14 -0300

    feat: ajustes manuais na aplicacao para corrigir erro de inicializacao

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 // create a REST API server with express
 import express from 'express';
 import { Request, Response } from 'express';
+import cors from 'cors';
 
-const app = express();
 const port = 3000;
+const app = express();
 
+// implement the most common middleware here, for example body-parser, cors, etc.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors()); 
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
